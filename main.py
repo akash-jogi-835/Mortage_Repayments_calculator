deeba2ff2d9fd9aa9d1465afcea6c92ac1c97ff7
akash-jogi-835 <149141998+akash-jogi-835@users.noreply.github.com>
2025-07-22 16:36:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("My Cool Chart")
+
+chart_data = pd.DataFrame(
+        np.random.randn(20,3),
+        columns = ['A', 'B', 'C'],
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
+
+
